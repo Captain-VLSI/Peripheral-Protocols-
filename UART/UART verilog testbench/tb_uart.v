950a07106f780f36504864bc9c7d5cf329408efa
GANESH S <165929138+Captain-VLSI@users.noreply.github.com>
2026-01-04 14:49:20 +0530

    Update tb_uart.v

--- UART/UART verilog testbench/tb_uart.v
+++ UART/UART verilog testbench/tb_uart.v
@@ -2,61 +2,83 @@ module uart_top_tb;
 
   reg clk;
   reg rst;
-  reg[7:0]data_in;
+  reg [7:0] data_in;
   reg wr_en;
-
+  
   wire tx;
   wire rdy;
   reg rdy_clr;
-  wire [7:0]dout;
-
+  wire [7:0] dout;
   wire busy;
 
-  uart_top dut (rst,data_in,wr_en,clk,rdy_clr,rdy,busy,dout);
+  uart_top dut (
+      .clk(clk),
+      .rst(rst),
+      .rx(tx),         
+      .wr_en(wr_en),
+      .din(data_in),
+      .rdy_clr(rdy_clr),
+      .tx(tx),
+      .busy(busy),
+      .rdy(rdy),
+      .dout(dout)
+  );
+
   initial begin
-    {clk,rst,data_in,rdy_clr}=0;
+    {clk, rst, data_in, rdy_clr, wr_en} = 0; 
   end
 
-  always #5 clk=~clk;
+  always #5 clk = ~clk;
 
   task send_byte (input [7:0] din);
     begin
-      @(negedge clk)
-      data_in=din;
-      wr_en=1'b1;
+      @(negedge clk);
+      data_in = din;
+      wr_en = 1'b1;
 
-      @(negedge clk)
-      wr_en=0;
-
+      @(negedge clk);
+      wr_en = 1'b0;
     end
+  endtask
 
-    task clear ready;
-      begin
-        rdy_clr=1'b1;
-        @(negedge clk)
-        rdy_clr=1'b0;
-      end
-    endtask
-
-    initial begin
-      @(negedge clk)
-      rst=1'b1;
-      @(negedge clk)
-      rst=1'b0;
-
-      send_byte(8'h41);
-      wait (!busy);
-      wait(rdy);
-      $display("received data is %h", dout);
-      clear_ready;
-
-      sendbyte(8'h55);
-      wait(!busy);
-      wait(rdy);
-      $display("received data is %h", dout);
-      clear_ready;
-
-      #400 $finish;
+  task clear_ready;
+    begin
+      rdy_clr = 1'b1;
+      @(negedge clk);
+      rdy_clr = 1'b0;
     end
-    endmodule
+  endtask
+
+
+  initial begin
+   
+    @(negedge clk);
+    rst = 1'b1;
+    @(negedge clk);
+    rst = 1'b0;
+
+    send_byte(8'h41);
     
+    wait(busy); 
+
+    wait(!busy);
+    wait(rdy);
+    
+    $display("Time: %0t | Sent: 41 | Received: %h", $time, dout);
+    clear_ready;
+
+    #100;
+
+    send_byte(8'h55);
+    
+    wait(busy);
+    wait(!busy);
+    wait(rdy);
+    
+    $display("Time: %0t | Sent: 55 | Received: %h", $time, dout);
+    clear_ready;
+
+    #400 $finish;
+  end
+
+endmodule
